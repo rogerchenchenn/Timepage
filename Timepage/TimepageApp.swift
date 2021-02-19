a25d7ce1755d02c1908996b04b817370ba00c748
Roger Chen <rogerchenshaoming@gmail.com>
2021-02-19 15:01:29 +0800

    Initial Commit

---
+++ Timepage/TimepageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimepageApp.swift
+//  Timepage
+//
+//  Created by Roger Chen on 2021/2/19.
+//
+
+import SwiftUI
+
+@main
+struct TimepageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
